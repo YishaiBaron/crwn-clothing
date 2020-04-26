a164f11030659d80c92af01cd5ea066999b3db68
Yishai Baron <yishai.baron@gmail.com>
2020-04-26 16:08:47 +0300

    broken our homepage to smaller compoments, finsh styled of menu compoment

---
+++ src/pages/homepage/homepage.compoment.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../../compoments/directory/directory.compoment'
+
+
+ const HomePage = () => (
+   
+        <div className='homepage'>
+       <Directory/>
+        </div>
+    
+);
+export default HomePage;
