e021b9e5d1531eb30c3dadfcb690193a00b38aa1
Yishai Baron <yishai.baron@gmail.com>
2020-04-28 16:29:42 +0300

    adding login compoment

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.compoment'
 import{Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.compoment'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.compoment'
 import Header from './compoments/header/header.component'
 
 
@@ -13,6 +14,8 @@ function App() {
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route path='/shop' component={ShopPage}/>
+    <Route path='/signin' component={SignInAndSignUpPage}/>
+
 
     </Switch>
     </div>
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.compoment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../../compoments/sign-in/sign-in.compoment'
+
+const SignInAndSignUpPage = () =>  (
+        <div>
+            <SignIn/>
+        </div>
+    )
+  export default SignInAndSignUpPage
+
