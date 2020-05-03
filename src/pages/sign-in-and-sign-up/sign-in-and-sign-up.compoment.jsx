f7cad88ccf834314a3824845327dc8270a8d3ed1
Yishai Baron <yishai.baron@gmail.com>
2020-05-03 17:18:26 +0300

    implement firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.compoment.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.compoment.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
 import SignIn from '../../compoments/sign-in/sign-in.compoment'
+import SignUp from '../../compoments/sign-up/sign-up.compoment'
+
 
 const SignInAndSignUpPage = () =>  (
-        <div>
+        <div className='sign-in-and-sign-up'>
             <SignIn/>
+            <SignUp/>
         </div>
     )
   export default SignInAndSignUpPage
