d0ca03d26331aa52473cabe031dae1c4ec3a4a45
Yishai Baron <yishai.baron@gmail.com>
2020-05-04 13:09:26 +0300

    update app to redicted to home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.compoment'
-import{Switch, Route} from 'react-router-dom';
+import{Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.compoment'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.compoment'
 import Header from './compoments/header/header.component'
@@ -44,20 +44,30 @@ render(){
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route path='/shop' component={ShopPage}/>
-    <Route path='/signin' component={SignInAndSignUpPage}/>
+    <Route exact path='/signin' 
+      render={()=> this.props.currentUser?(
+        <Redirect to= '/' />
+        ):(
+        <SignInAndSignUpPage/>
+        )
 
+      }
+    />
 
     </Switch>
     </div>
   );
 }
 }
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
